f0d09e0f1edb0dd0acda94bf229181dc3627e40a
Nils Hartmann <nils@nilshartmann.net>
2025-03-10 11:41:09 +0100

    Multiple ways of rendering conditionally

--- workspace/src/components/CardImage.tsx
+++ workspace/src/components/CardImage.tsx
@@ -47,4 +47,43 @@ export default function CardImage({
   );
 }
 
-// export function CardList() {}
+// export default function CardImage({
+//                                     name,
+//                                     caption,
+//                                     decoration,
+//                                   }: CardImageProps) {
+//   // console.log(props.name)
+//   // console.log(name);
+//
+//   const imageSrc = `/images/${name}`;
+//   const rootClassName = decoration
+//     ? "CardImage CardImageDecoration"
+//     : "CardImage";
+//
+//   let captionElement;
+//   if (caption) {
+//     captionElement = <div className="CardImageCaption">{caption}</div>;
+//   }
+//
+//   const captionElement2 =
+//     caption === undefined ? null : (
+//       <div className="CardImageCaption">{caption}</div>
+//     );
+//
+//   // JSX
+//   return (
+//     <div className={rootClassName}>
+//       <img src={imageSrc} />
+//       {!!caption && <div className="CardImageCaption">{caption}</div>}
+//
+//       {caption === undefined ? null : (
+//         <div className="CardImageCaption">{caption}</div>
+//       )}
+//       {caption !== undefined && (
+//         <div className="CardImageCaption">{caption}</div>
+//       )}
+//       {captionElement}
+//       {captionElement2}
+//     </div>
+//   );
+// }
