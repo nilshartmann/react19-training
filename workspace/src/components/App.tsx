6221fcbde87f43a778c2a6037df277fd1cfecba2
Nils Hartmann <nils@nilshartmann.net>
2025-03-10 11:08:29 +0100

    02a: Props and conditions

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -3,7 +3,8 @@ import CardImage from "./CardImage.tsx";
 export default function App() {
   return (
     <div className={"container mx-auto pt-8"}>
-      <CardImage />
+      <CardImage name={"03.png"} decoration={false} caption={"Hello Image"} />
+      <CardImage name={"04.png"} decoration={false} />
     </div>
   );
 }
--- workspace/src/components/CardImage.tsx
+++ workspace/src/components/CardImage.tsx
@@ -1,9 +1,67 @@
-export default function CardImage() {
+// properties or props
+//
+// <CardImage name="/images/01.png" caption="Hello React" decoration={true} />
+//
+//  props
+//  { name: "/images/01.png",
+//    caption: "Hello React",
+//    decoration: true
+//  }
+
+// "Type Alias" in Typescript
+type CardImageProps = {
+  name: string;
+  caption?: string;
+  decoration: boolean;
+};
+
+// type Boolean = boolean
+
+// interface ICardImageProps  {
+//   name: string;
+//   caption?: string;
+//   decoration: boolean;
+// }
+
+// destrucutring operator
+// export default function CardImage(props: CardImageProps)
+export default function CardImage({
+  name,
+  caption,
+  decoration,
+}: CardImageProps) {
+  // console.log(props.name)
+  // console.log(name);
+
+  const imageSrc = `/images/${name}`;
+  const rootClassName = decoration
+    ? "CardImage CardImageDecoration"
+    : "CardImage";
+
+  let captionElement;
+  if (caption) {
+    captionElement = <div className="CardImageCaption">{caption}</div>;
+  }
+
+  const captionElement2 =
+    caption === undefined ? null : (
+      <div className="CardImageCaption">{caption}</div>
+    );
+
   // JSX
   return (
-    <div className={"CardImage"}>
-      <img src="/images/01.png" />
-      <div className="CardImageCaption">My Caption</div>
+    <div className={rootClassName}>
+      <img src={imageSrc} />
+      {!!caption && <div className="CardImageCaption">{caption}</div>}
+
+      {caption === undefined ? null : (
+        <div className="CardImageCaption">{caption}</div>
+      )}
+      {caption !== undefined && (
+        <div className="CardImageCaption">{caption}</div>
+      )}
+      {captionElement}
+      {captionElement2}
     </div>
   );
 }
